59e596a5de3dd04ce4514b2b60ac6a8c99cc55d3
Timo Ruokonen <ruokonen@Timo-Ruokonens-MacBook-Pro.local>
2012-10-09 23:22:47 +0300

    Added generic command and literal classes

--- project.py
+++ project.py
@@ -1,23 +1,86 @@
-class line:
-    def __init__(self, msg):
-        self.line = 'print "' + msg + '"' 
+import random
+
+class literal:
+    registered_literals = list()
+
+    @staticmethod
+    def register_literal(new_literal):
+        literal.registered_literals.append(new_literal)
+        print "Registered literal: " + str(new_literal)
+
+    @staticmethod
+    def generate(typeof):
+        while(True):
+            lit = literal.registered_literals[random.randrange(len(literal.registered_literals))]
+            #print "Comparing: " + str(type(lit)) + " to " + str(typeof)
+            if (type(lit) == typeof):
+                break
+        new_literal = literal()
+        new_literal.literal = lit
+        return new_literal
     
     def to_s(self):
-        return self.line
+        if (type(self.literal) == str):
+            return '"' + self.literal + '"' 
+        else:
+            return str(self.literal)
+
+class command:
+    registered_commands = list()
+
+    @staticmethod
+    def register_command(new_command):
+        command.registered_commands.append(new_command)
+        print "Registered command: " + new_command[0]
+
+    @staticmethod
+    def generate():
+        #create a new command and randomize its type
+        new_command = command()
+        new_command.command = command.registered_commands[random.randrange(len(command.registered_commands))]
+
+        #create parameters to command if needed
+        new_command.parameters = []
+        if (len(new_command.command[2]) > 0):
+            for param in new_command.command[2]:
+                new_command.parameters.append(literal.generate(param))
+        return new_command    
+        
+    def to_s(self):
+        retval = self.command[0] + '('
+        first = True
+        for param in self.parameters:
+            if (first == False):
+                retval += ', '
+            else:
+                first = False
+            retval +=  param.to_s() 
+
+        retval += ')'
+        return retval
+ 
 
 
 class block:
+    registered_blocks = list()
+
+    @staticmethod
+    def register_block(new_block):
+        block.registered_blocks.append(new_block)
+        print "Registered block: " + new_block
+
     def __init__(self):
-        line1 = line("Kaljaa!!")
-        line2 = line("Kebabbia!!")
-        self.lines = list()
-        self.lines.append(line1)
-        self.lines.append(line2) 
+        self.block = block.registered_blocks[random.randrange(len(self.registered_blocks))]
+
+        self.commands = list()
+        self.commands.append(command.generate())
+        self.commands.append(command.generate())
+        self.commands.append(command.generate())
     
     def to_s(self):
-        ret = "if (True):"
-        for line in self.lines:
-            ret += "\n\t" + line.to_s()
+        ret = self.block + ":"
+        for c in self.commands:
+            ret += "\n\t" + c.to_s()
         return ret
 
 
@@ -32,6 +95,15 @@ class project:
 
     def start(self):
         print "Starting..." 
+
+        literal.register_literal("Kaljaa!!")
+        literal.register_literal("Kebabbia??")
+        literal.register_literal(1001)        
+        block.register_block("if (True)");
+        block.register_block("if (False)");
+        command.register_command(["print", None, [type("a")]])
+        command.register_command(["print", None, [type(1)]])
+
         print "Generating code for generation one:"
         generator = code_generator()
         code = generator.generate()
