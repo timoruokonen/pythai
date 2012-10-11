69f4a3e7fb919186886032a2fc9f638154f2a23b
Timo Ruokonen <ruokonen@Timo-Ruokonens-MacBook-Pro.local>
2012-10-11 19:36:06 +0300

    Added global variables

--- project.py
+++ project.py
@@ -1,10 +1,37 @@
 import random
 
+class global_variable:
+    registered_global_variables = list()
+
+    @staticmethod
+    def register(name, typeof):
+        global_variable.registered_global_variables.append([name, typeof])
+
+    @staticmethod
+    def generate(typeof):
+        while(True):
+            var = global_variable.registered_global_variables[random.randrange(len(global_variable.registered_global_variables))]
+            print "Comparing: " + str(var[1]) + " to " + str(typeof)
+            if (var[1] == typeof):
+                break
+        return var[0]
+        
+    @staticmethod
+    def to_s():
+        retval = ""
+        for variable in global_variable.registered_global_variables:
+            #get the type as string from type
+            print str(variable[1])
+            retval += variable[0] + " = " + str(variable[1]).split(".")[1] + "()"
+            retval += "\n"
+        return retval
+
+
 class literal:
     registered_literals = list()
 
     @staticmethod
-    def register_literal(new_literal):
+    def register(new_literal):
         literal.registered_literals.append(new_literal)
         print "Registered literal: " + str(new_literal)
 
@@ -29,7 +56,7 @@ class command:
     registered_commands = list()
 
     @staticmethod
-    def register_command(new_command):
+    def register(new_command):
         command.registered_commands.append(new_command)
         print "Registered command: " + new_command[0]
 
@@ -41,13 +68,19 @@ class command:
 
         #create parameters to command if needed
         new_command.parameters = []
-        if (len(new_command.command[2]) > 0):
-            for param in new_command.command[2]:
+        if (len(new_command.command[3]) > 0):
+            for param in new_command.command[3]:
                 new_command.parameters.append(literal.generate(param))
         return new_command    
         
     def to_s(self):
-        retval = self.command[0] + '('
+        retval = ""
+        #if object was given, add that to the call
+        if (self.command[2] != None):
+            variable = global_variable.generate(self.command[2])
+            retval += variable + "."
+        retval += self.command[0] + '('
+        #add parameters to the call
         first = True
         for param in self.parameters:
             if (first == False):
@@ -63,9 +96,10 @@ class command:
 
 class block:
     registered_blocks = list()
+    maximumCommands = 5
 
     @staticmethod
-    def register_block(new_block):
+    def register(new_block):
         block.registered_blocks.append(new_block)
         print "Registered block: " + new_block
 
@@ -73,9 +107,8 @@ class block:
         self.block = block.registered_blocks[random.randrange(len(self.registered_blocks))]
 
         self.commands = list()
-        self.commands.append(command.generate())
-        self.commands.append(command.generate())
-        self.commands.append(command.generate())
+        for i in range(1 + random.randrange(block.maximumCommands)):
+            self.commands.append(command.generate())
     
     def to_s(self):
         ret = self.block + ":"
@@ -85,28 +118,56 @@ class block:
 
 
 class code_generator:
+    maximumBlocks = 2
+
     def __init__(self):
-        self.blocks = block()
+        self.blocks = list()
 
     def generate(self):
-        return self.blocks.to_s() 
+        for i in range(1 + random.randrange(code_generator.maximumBlocks)):    
+            self.blocks.append(block()) 
+
+    def to_s(self):
+        retval = "";
+        retval += global_variable.to_s()
+        for block in self.blocks:
+            retval += block.to_s()
+            retval += "\n"
+        return retval
+
+
+class dummy_class:
+    @staticmethod
+    def operation1(text):
+        print "dummy_class: operation1 " + text
+
+    @staticmethod
+    def operation2(number):
+        print "dummy_calss: operation2 " + str(number)
 
 class project:
 
     def start(self):
         print "Starting..." 
 
-        literal.register_literal("Kaljaa!!")
-        literal.register_literal("Kebabbia??")
-        literal.register_literal(1001)        
-        block.register_block("if (True)");
-        block.register_block("if (False)");
-        command.register_command(["print", None, [type("a")]])
-        command.register_command(["print", None, [type(1)]])
+        gv1 = global_variable.register("dummy", dummy_class)
+
+        literal.register("Kaljaa!!")
+        literal.register("Kebabbia??")
+        literal.register("Vodaa...?")
+        literal.register("Jaloviinaa?")
+        literal.register(1001)        
+        block.register("if (True)");
+        #block.register("if (False)");
+        command.register(["print", None, None, [type("a")]])
+        command.register(["print", None, None, [type(1)]])
+        command.register(["operation1", None, dummy_class, [type("a")]])
+        #command.register(["dummy.operation2", None, None, [type(1)]])
 
         print "Generating code for generation one:"
         generator = code_generator()
-        code = generator.generate()
+        generator.generate()
+        code = generator.to_s()
         print "Code:"
         print "-" * 40
         print code 
